49cb51666380f5a2a81a4dc89ef47fc4d1b3c0d6
Nicola Fiorella <nicola.fiorella@gmail.com>
2024-10-09 21:06:49 +0200

    move options to lazy.lua file

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -14,7 +14,20 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 	end
 end
 vim.opt.rtp:prepend(lazypath)
+-- vim options
 vim.g.mapleader = " "
+vim.opt.relativenumber = true
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.wrap = false
+vim.opt.expandtab = true --turn tabs to spaces
+vim.opt.tabstop = 4 --1 tab = 4 spaces
+vim.opt.shiftwidth = 4
+vim.opt.virtualedit = "block"
+vim.opt.inccommand = "split"
+vim.opt.clipboard = "unnamedplus"
+vim.opt.ignorecase = true
+vim.opt.termguicolors = true
 
 -- Setup lazy.nvim
 require("lazy").setup({
