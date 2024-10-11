ea3eea21b9eaee1e692466aa642a99f92d16a1d3
Nicola <nicola.fiorella@prima.it>
2024-10-11 13:44:27 +0200

    add git plugins: fugitive and gitsigns

---
+++ lua/plugins/git.lua
@@ -0,0 +1,18 @@
+return {
+	{
+		-- use :Git to call any git command from within nvim
+		"tpope/vim-fugitive",
+	},
+    {
+		"lewis6991/gitsigns.nvim",
+		opts = {
+			signs = {
+				add = { text = "+" },
+				change = { text = "~" },
+				delete = { text = "_" },
+				topdelete = { text = "‾" },
+				changedelete = { text = "~" },
+			},
+		},
+	},
+}
