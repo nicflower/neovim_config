4cfd8e31604998f5adcae1b6ec82869f622825ad
Nicola <nicola.fiorella@prima.it>
2024-11-11 12:24:55 +0100

    add keymap (in gitsigns) to navigate git hunks

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -3,7 +3,7 @@ return {
 		-- use :Git to call any git command from within nvim
 		"tpope/vim-fugitive",
 	},
-    {
+	{
 		"lewis6991/gitsigns.nvim",
 		opts = {
 			signs = {
@@ -13,6 +13,25 @@ return {
 				topdelete = { text = "‾" },
 				changedelete = { text = "~" },
 			},
+			on_attach = function(bufnr)
+                local gitsigns = require('gitsigns')
+				-- Navigation
+				vim.keymap.set("n", "]c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "]c", bang = true })
+					else
+						gitsigns.nav_hunk("next")
+					end
+				end)
+
+				vim.keymap.set("n", "[c", function()
+					if vim.wo.diff then
+						vim.cmd.normal({ "[c", bang = true })
+					else
+						gitsigns.nav_hunk("prev")
+					end
+				end)
+			end,
 		},
 	},
 }
