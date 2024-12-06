55dc011c4964b4633d2630462ec98c4b7105ce99
Nicola <nicola.fiorella@prima.it>
2024-12-06 15:24:17 +0100

    elm lsp

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -10,7 +10,9 @@ return {
 			require("lspconfig").elixirls.setup({
 				cmd = { vim.fn.getenv("HOME") .. "/src/elixir-ls-v0.24.1/language_server.sh" },
 			})
-            require("lspconfig").zls.setup({})
+            require("lspconfig").elmls.setup({
+                cmd = {"elm-language-server"},
+            })
 
 			vim.api.nvim_create_autocmd("LspAttach", {
 				group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
