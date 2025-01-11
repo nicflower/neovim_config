511b8fa5e311774fd261c3db4df61b4473b47b96
Nicola Fiorella <nicola.fiorella@gmail.com>
2025-01-11 16:21:15 +0100

    add mini statusline

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,22 +1,23 @@
 return {
-	{
-		"echasnovski/mini.nvim",
-		version = false,
-		config = function()
-			require("mini.animate").setup()
-			local notify = require("mini.notify")
-			notify.setup()
-			vim.notify = notify.make_notify({
-				ERROR = { duration = 5000 },
-				WARN = { duration = 4000 },
-				INFO = { duration = 3000 },
-			})
-			require("mini.pairs").setup()
-			require("mini.files").setup()
-			require("mini.comment").setup()
-			vim.keymap.set("n", "<leader>ex", function()
-				MiniFiles.open()
-			end, {})
-		end,
-	},
+    {
+        "echasnovski/mini.nvim",
+        version = false,
+        config = function()
+            require("mini.animate").setup()
+            local notify = require("mini.notify")
+            notify.setup()
+            vim.notify = notify.make_notify({
+                ERROR = { duration = 15000 },
+                WARN = { duration = 10000 },
+                INFO = { duration = 5000 },
+            })
+            require("mini.pairs").setup()
+            require("mini.files").setup()
+            require("mini.comment").setup()
+            vim.keymap.set("n", "<leader>ex", function()
+                MiniFiles.open()
+            end, {})
+            require("mini.statusline").setup()
+        end,
+    },
 }
