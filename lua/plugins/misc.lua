89114558978350798599049aeac5f30cd89e640f
Nicola Fiorella <nicola.fiorella@gmail.com>
2025-01-15 11:37:17 +0100

    add dropbar/breadcrumbs

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -1,11 +1,13 @@
 return {
+    -- fade unused pane
     { "TaDaa/vimade" },
     {
         "mbbill/undotree",
         config = function()
-            vim.keymap.set("n", "<leader>un", vim.cmd.UndotreeToggle)
+            vim.keymap.set("n", "<leader>un", vim.cmd.UndotreeToggle, { desc = "Toggle Undotree" })
         end,
     },
+    -- markdown files preview inside nvim
     {
         "OXY2DEV/markview.nvim",
         lazy = false,
@@ -29,5 +31,20 @@ return {
                 desc = "Buffer Local Keymaps (which-key)",
             },
         },
+    },
+    -- breadcrumbs in topbar
+    {
+        'Bekaboo/dropbar.nvim',
+        requires = {
+            'nvim-telescope/telescope-fzf-native.nvim',
+            run = 'make'
+        },
+        config = function()
+            local dropbar_api = require('dropbar.api')
+            vim.keymap.set('n', '<leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
+            vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
+            vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
+        end
     }
+
 }
