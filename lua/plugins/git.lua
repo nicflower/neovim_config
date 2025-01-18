dd67e36a59dac8b4efc24d1f1546d5a89aef867d
Nicola Fiorella <nicola.fiorella@gmail.com>
2025-01-18 12:35:30 +0100

    add description for gitsign keymaps

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,37 +1,37 @@
 return {
-	{
-		-- use :Git to call any git command from within nvim
-		"tpope/vim-fugitive",
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-			on_attach = function(bufnr)
+    {
+        -- use :Git to call any git command from within nvim
+        "tpope/vim-fugitive",
+    },
+    {
+        "lewis6991/gitsigns.nvim",
+        opts = {
+            signs = {
+                add = { text = "+" },
+                change = { text = "~" },
+                delete = { text = "_" },
+                topdelete = { text = "‾" },
+                changedelete = { text = "~" },
+            },
+            on_attach = function(bufnr)
                 local gitsigns = require('gitsigns')
-				-- Navigation
-				vim.keymap.set("n", "]c", function()
-					if vim.wo.diff then
-						vim.cmd.normal({ "]c", bang = true })
-					else
-						gitsigns.nav_hunk("next")
-					end
-				end)
+                -- Navigation
+                vim.keymap.set("n", "]c", function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "]c", bang = true })
+                    else
+                        gitsigns.nav_hunk("next")
+                    end
+                end, { desc = "Next git diff chunk" })
 
-				vim.keymap.set("n", "[c", function()
-					if vim.wo.diff then
-						vim.cmd.normal({ "[c", bang = true })
-					else
-						gitsigns.nav_hunk("prev")
-					end
-				end)
-			end,
-		},
-	},
+                vim.keymap.set("n", "[c", function()
+                    if vim.wo.diff then
+                        vim.cmd.normal({ "[c", bang = true })
+                    else
+                        gitsigns.nav_hunk("prev")
+                    end
+                end, { desc = "Previous git diff chunk" })
+            end,
+        },
+    },
 }
