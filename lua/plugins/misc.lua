18ed07a9ea3eaee3b5b88c19b89cbe2e45f61d27
Nicola Fiorella <nicola.fiorella@prima.it>
2025-04-01 08:51:15 +0200

    remove breadcrumbs plugin

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -32,18 +32,4 @@ return {
             },
         },
     },
-    -- breadcrumbs in topbar
-    {
-        'Bekaboo/dropbar.nvim',
-        requires = {
-            'nvim-telescope/telescope-fzf-native.nvim',
-            run = 'make'
-        },
-        config = function()
-            local dropbar_api = require('dropbar.api')
-            vim.keymap.set('n', '<leader>;', dropbar_api.pick, { desc = 'Pick symbols in winbar' })
-            vim.keymap.set('n', '[;', dropbar_api.goto_context_start, { desc = 'Go to start of current context' })
-            vim.keymap.set('n', '];', dropbar_api.select_next_context, { desc = 'Select next context' })
-        end
-    },
 }
