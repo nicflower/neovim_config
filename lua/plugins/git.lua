b00a20bc3d50cf21c97970ecd71ef17ea275a0ae
Nicola Fiorella <nicola.fiorella@prima.it>
2025-04-01 08:43:06 +0200

    add fugitive commands for vertical status and logs

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,6 +2,10 @@ return {
     {
         -- use :Git to call any git command from within nvim
         "tpope/vim-fugitive",
+        config = function ()
+           vim.api.nvim_create_user_command('VG', 'vertical G', { desc = "Open fugitive status vertically"}) 
+           vim.api.nvim_create_user_command('VGlog', 'vertical G log', { desc = "Open fugitive log vertically"}) 
+        end,
     },
     {
         "lewis6991/gitsigns.nvim",
