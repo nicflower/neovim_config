6d23a78c7dfd0a2b287b4b66f534cbf1a49f4403
Nicola Fiorella <nicola.fiorella@gmail.com>
2026-02-09 08:56:46 +0100

    improve oil config

--- lua/plugins/misc.lua
+++ lua/plugins/misc.lua
@@ -19,28 +19,43 @@ return {
   },
   {
     'stevearc/oil.nvim',
-    config = function()
-      require("oil").setup({
-        default_file_explorer = true,
-        delete_to_trash = true,
-        view_options = {
-          show_hidden = true,
-          natural_order = true,
-          is_always_hidden = function(name, _)
-            return name == ".." or name == ".git"
-          end
-        },
-      })
-      vim.keymap.set("n", "<leader>ex", function() require("oil").open(nil, nil, nil) end,
-        { desc = "Open Oil" })
-      vim.keymap.set("n", "<leader>exf", function() require("oil").open_float(nil, nil, nil) end,
-        { desc = "Open Oil" })
-      vim.keymap.set("n", "<leader>ew", function() require("oil").open(vim.fn.getcwd(), nil, nil) end,
-        { desc = "Open Oil in current directory" })
-    end,
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    lazy = false,
+    keys = {
+      {
+        "<leader>ex",
+        function()
+          require("oil").open(nil, nil, nil)
+        end,
+        desc = "Open Oil"
+      },
+      {
+        "<leader>exf",
+        function()
+          require("oil").open(nil, nil, nil)
+        end,
+        desc = "Open Oil floating"
+      },
+      {
+        "<leader>ew",
+        function()
+          require("oil").open(vim.fn.getcwd(), nil, nil)
+        end,
+        desc = "Open Oil in current directory"
+      }
 
+    },
+    opts = {
+      default_file_explorer = true,
+      delete_to_trash = true,
+      view_options = {
+        show_hidden = true,
+        natural_order = true,
+        is_always_hidden = function(name, _)
+          return name == ".." or name == ".git"
+        end
+      },
+    },
+    lazy = false,
   },
   -- ufo: folding
   {
