206564ca70ccc206787f0dd149ee41af74b21ea5
Nicola Fiorella <nicola.fiorella@prima.it>
2026-04-15 09:25:51 +0200

    add treesitter context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -53,7 +53,7 @@ return {
             -- mapping query_strings to modes.
             selection_modes = {
               ["@parameter.outer"] = "v", -- charwise
-              ["@function.outer"] = "v", -- charwise
+              ["@function.outer"] = "v",  -- charwise
               ["@class.outer"] = "<c-v>", -- blockwise
             },
             -- If you set this to `true` (default is `false`) then any textobject is
@@ -74,4 +74,31 @@ return {
   {
     "nvim-treesitter/nvim-treesitter-textobjects",
   },
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    opts = {
+      enable = true,            -- Enable this plugin (Can be enabled/disabled later via commands)
+      multiwindow = false,      -- Enable multiwindow support.
+      max_lines = 0,            -- How many lines the window should span. Values <= 0 mean no limit.
+      min_window_height = 0,    -- Minimum editor window height to enable context. Values <= 0 mean no limit.
+      line_numbers = true,
+      multiline_threshold = 20, -- Maximum number of lines to show for a single context
+      trim_scope = 'outer',     -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
+      mode = 'cursor',          -- Line used to calculate context. Choices: 'cursor', 'topline'
+      -- Separator between context and content. Should be a single character string, like '-'.
+      -- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
+      separator = '-',
+      zindex = 20,     -- The Z-index of the context window
+      on_attach = nil, -- (fun(buf: integer): boolean) return false to disable atta}
+    },
+    config = function()
+      local tresitter_context = require("treesitter-context")
+      vim.keymap.set("n", "[k", function()
+        tresitter_context.go_to_context(vim.v.count1)
+      end, { silent = true, desc = "Go to Context" })
+      vim.keymap.set("n", "<leader>tk", function()
+        tresitter_context.toggle()
+      end, { silent = true, desc = "Toggle Context" })
+    end
+  }
 }
