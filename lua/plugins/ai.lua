b9d74a284e62391d45c2984d1d30460e93878f14
Nicola Fiorella <nicola.fiorella@prima.it>
2026-04-15 09:22:20 +0200

    replace copilotchat with codecompanion

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -7,33 +7,17 @@ return {
     end
   },
   {
-    "CopilotC-Nvim/CopilotChat.nvim",
+    "olimorris/codecompanion.nvim",
+    version = "^19.0.0",
+    opts = {},
     dependencies = {
-      { "nvim-lua/plenary.nvim", branch = "master" },
-    },
-    build = "make tiktoken",
-    opts = {
-      model = 'gpt-4.1',       -- AI model to use
-      temperature = 0.1,       -- Lower = focused, higher = creative
-      window = {
-        layout = 'vertical',   -- 'vertical', 'horizontal', 'float'
-        width = 0.5,           -- 50% of screen width
-      },
-      auto_insert_mode = true, -- Enter insert mode when opening
+      "nvim-lua/plenary.nvim",
+      "nvim-treesitter/nvim-treesitter",
     },
     config = function()
-      vim.api.nvim_create_autocmd('BufEnter', {
-        pattern = 'copilot-*',
-        callback = function()
-          vim.opt_local.relativenumber = false
-          vim.opt_local.number = false
-          vim.opt_local.conceallevel = 0
-        end,
-      })
-      local copilot_chat = require("CopilotChat")
-      vim.keymap.set("n", "<leader>cc", copilot_chat.toggle, { desc = "[S]earch [F]iles" })
+      local codecompanion = require("codecompanion")
+
+      vim.keymap.set("n", "<leader>cc", codecompanion.toggle, { desc = "[C]ode [C]ompanion" })
     end
-
-
-  }
+  },
 }
