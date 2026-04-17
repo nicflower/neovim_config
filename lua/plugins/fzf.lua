babedae124f6d65fb51d6d119bad6834698a7cbe
Nicola Fiorella <nicola.fiorella@prima.it>
2026-04-17 09:56:48 +0200

    fix typos in fzf config

--- lua/plugins/fzf.lua
+++ lua/plugins/fzf.lua
@@ -22,10 +22,10 @@ return {
     })
     vim.keymap.set("n", "<leader>sf", fzf.files, { desc = "[S]earch [F]iles" })
     vim.keymap.set("n", "<leader>sg", fzf.live_grep, { desc = "[S]earch by [G]rep" })
-    vim.keymap.set("n", "<leader>sr", fzf.live_grep, { desc = "[S]earch [R]esume" })
+vim.keymap.set("n", "<leader>sr", fzf.resume, { desc = "[S]earch [R]esume" })
     vim.keymap.set("n", "<leader>s.", fzf.oldfiles, { desc = "[S]earch Recent Files" })
     vim.keymap.set("n", "<leader><leader>", fzf.buffers, { desc = "Search Recent Buffers" })
-    vim.keymap.set("n", "<leader>/", fzf.blines, { desc = "[S]earch in cucrent Buffer" })
+vim.keymap.set("n", "<leader>/", fzf.blines, { desc = "[S]earch in current Buffer" })
     vim.keymap.set("n", "<leader>sw", fzf.grep_cword, { desc = "[S]earch Current [W]ord" })
   end
 }
